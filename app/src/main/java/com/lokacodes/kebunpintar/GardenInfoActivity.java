01e10bff9ca55d74717f147665ff6f29b21151c9
Lokacodes <talloka21.mhs@akb.ac.id>
2023-03-18 14:59:29 +0700

    first commit

---
+++ app/src/main/java/com/lokacodes/kebunpintar/GardenInfoActivity.java
@@ -0,0 +1,14 @@
+package com.lokacodes.kebunpintar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GardenInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_garden_info);
+    }
+}
